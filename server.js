adeccb78547a9150a7af5e19b0b21e817fdae3cb
Himadri Barad <118922878+b-himadri@users.noreply.github.com>
2025-07-17 10:07:52 +0530

    Complete backend commit

---
+++ server.js
@@ -0,0 +1,32 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routes/userRoute.js";
+import productRouter from "./routes/productRoute.js";
+import orderRouter from "./routes/orderRoute.js"
+import cors from "cors";
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+const mongo_url = encodeURIComponent(process.env.MONGO_URL);
+
+// mongoose.connect(`mongodb://localhost:27017/merncafe`).then(() => {
+//   app.listen(8080, () => {
+//     console.log("Server started");
+//   });
+// });
+
+mongoose
+  .connect(
+    `${mongo_url}`
+  )
+  .then(() => {
+    app.listen(8080, () => {
+      console.log("Server started");
+    });
+  });
+
+app.use("/api/users", userRouter);
+app.use("/api/products", productRouter);
+app.use("/api/orders", orderRouter)
