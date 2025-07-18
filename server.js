e1c40f1de544d5522ba187493e1b461c91fbcda5
Himadri Barad <118922878+b-himadri@users.noreply.github.com>
2025-07-18 10:35:29 +0530

    updated server.js

--- server.js
+++ server.js
@@ -1,24 +1,49 @@
+// import express from "express";
+// import mongoose from "mongoose";
+// import dotenv from "dotenv";
+// import userRouter from "./routes/userRoute.js";
+// import productRouter from "./routes/productRoute.js";
+// import orderRouter from "./routes/orderRoute.js"
+// import cors from "cors";
+// dotenv.config();
+// const app = express();
+// app.use(cors());
+// app.use(express.json());
+// const mongo_url = process.env.MONGO_URL;
+
+// mongoose
+//   .connect(mongo_url)
+//   .then(() => {
+//     app.listen(8080, () => {
+//       console.log("Server started");
+//     });
+//   });
+
+// app.use("/api/users", userRouter);
+// app.use("/api/products", productRouter);
+// app.use("/api/orders", orderRouter)
+
+
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/userRoute.js";
 import productRouter from "./routes/productRoute.js";
-import orderRouter from "./routes/orderRoute.js"
+import orderRouter from "./routes/orderRoute.js";
 import cors from "cors";
+
 dotenv.config();
+
 const app = express();
 app.use(cors());
 app.use(express.json());
-const mongo_url = process.env.MONGO_URL;
 
-mongoose
-  .connect(mongo_url)
-  .then(() => {
-    app.listen(8080, () => {
-      console.log("Server started");
-    });
-  });
+mongoose.connect(process.env.MONGO_URL);
 
+// Routes
 app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
-app.use("/api/orders", orderRouter)
+app.use("/api/orders", orderRouter);
+
+// ✅ Export app (no listen)
+export default app;
